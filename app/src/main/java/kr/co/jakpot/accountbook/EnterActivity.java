16f933b6b4861c3d9a598e31fec485f497347632
jungyunmin <jungyunmin@nate.com>
2017-03-28 15:45:25 +0900

    테스트 수정

--- app/src/main/java/kr/co/jakpot/accountbook/EnterActivity.java
+++ app/src/main/java/kr/co/jakpot/accountbook/EnterActivity.java
@@ -9,6 +9,6 @@ public class EnterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enter);
-        System.out.println("22");
+
     }
 }
