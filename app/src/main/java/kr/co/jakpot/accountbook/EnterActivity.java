c3b6c72882519d0a22b70a4ecacc70fd635a5459
jungyunmin <jungyunmin@nate.com>
2017-04-06 17:14:19 +0900

    111

--- app/src/main/java/kr/co/jakpot/accountbook/EnterActivity.java
+++ app/src/main/java/kr/co/jakpot/accountbook/EnterActivity.java
@@ -2,13 +2,19 @@ package kr.co.jakpot.accountbook;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.Spinner;
 
 public class EnterActivity extends AppCompatActivity {
-
+    private Spinner m_spinnerGroup = null;
+    private Button m_btnEnter = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enter);
+
+        m_spinnerGroup = (Spinner) findViewById(R.id.spinnerGroup);
+        m_btnEnter = (Button) findViewById(R.id.buttonEnter);
 
     }
 }
